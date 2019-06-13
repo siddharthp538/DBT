187b553c7c9e6858760a0440df236577f818404a
siddharthp538 <svpandey86@gmail.com>
2019-06-13 09:29:20 +0530

    node server for token added

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/token', async (req,res)=> {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    otp %= 10000;
+    console.log(otp)
+    try {
+  
+      const bodyToSend = {
+        apikey: 'DZ5614KZ864GAY8EYARRMSNG3UMCHYVB',
+        secret: '0N05X4PUQ9WNSTWI',
+        usetype: 'stage',
+        phone: req.body.phone,
+        message: `Your One Time Password is ${otp}`,
+        senderid: 'varsha'
+      }
+      unirest.post(`http://www.way2sms.com/api/v1/sendCampaign`).send(bodyToSend).strictSSL(false).end(async (response) => {
+        console.log(bodyToSend)
+      })
+      return res.status(200).send({
+        message: otp
+      });
+  
+    } catch (error) {
+      console.log(error.message);
+      console.log(JSON.stringify(error))
+      return res.status(400).send({
+        message: error.message
+      });
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Node server running on port 5000....');
+})
