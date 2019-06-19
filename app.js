22a02f1886bbd2146ce459c424cd0530a3927821
siddharthp538 <svpandey86@gmail.com>
2019-06-19 15:26:30 +0530

    store otp added

--- app.js
+++ app.js
@@ -47,6 +47,44 @@ app.post('/token', async (req,res)=> {
     }
 });
 
+app.post('/storeOTP', (req,res)=>{
+  let otp = Math.floor(100000 + Math.random() * 900000);
+  otp %= 10000;
+  try {   
+
+    let bodyToSend = {
+      apikey: 'DZ5614KZ864GAY8EYARRMSNG3UMCHYVB',
+      secret: '0N05X4PUQ9WNSTWI',
+      usetype: 'stage',
+      phone: req.body.beneficiaryPhoneNumber,
+      message: `Your One Time Password is ${otp}`,
+      senderid: 'varsha'
+    }
+    unirest.post(`http://www.way2sms.com/api/v1/sendCampaign`).send(bodyToSend).strictSSL(false).end(async (response) => {
+      console.log(bodyToSend)
+    });
+    bodyToSend = {
+      beneficiaryAadhaarNo : req.body.beneficiaryAadhaarNo,
+      beneficiaryPhoneNumber : req.body.beneficiaryPhoneNumber,
+      tokenHash : hash
+    }
+    unirest.post(`http://localhost:3000/api/CalculateToken`).send(bodyToSend).strictSSL(false).end(async (response) => {
+      console.log(response)
+    });      
+    return res.status(200).send({
+      message: otp
+    });
+
+  } catch (error) {
+    console.log(error.message);
+    console.log(JSON.stringify(error))
+    return res.status(400).send({
+      message: error.message
+    });
+  }    
+});
+
+
 app.listen(5000, () => {
     console.log('Node server running on port 5000....');
 })
